85f257e176c757b0157e4178b59dd56bb59d357e
engiusep <enzo74giuseppin@gmail.com>
2026-04-24 15:31:56 +0200

    accept friends request > OK

--- api/friends/urls.py
+++ api/friends/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SendFriendRequest
+from .views import SendFriendRequest, AcceptFriendRequest
 
 
 
 urlpatterns = [
     path('request/',SendFriendRequest.as_view(),name='send-friend-request'),
+    path('request/<int:friendship_id>/accept/', AcceptFriendRequest.as_view(), name='accept-friend-request'),
 ]
--- api/friends/views.py
+++ api/friends/views.py
@@ -30,3 +30,25 @@ class SendFriendRequest(APIView):
         friendship = Friendship.objects.create(sender=request.user, receiver=receiver)
         serializer = FriendshipSerializer(friendship)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AcceptFriendRequest(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self,request,friendship_id):
+        
+        try:
+            friendship = Friendship.objects.get(id=friendship_id)
+        except Friendship.DoesNotExist:
+            return Response({'error': 'Demande introuvable'},status=status.HTTP_404_NOT_FOUND)
+        
+        if friendship.receiver != request.user:
+            return Response({'error': 'Tu n\'es pas autorisé'},status=status.HTTP_403_FORBIDDEN)
+        
+        if friendship.status != 'pending':
+            return Response({'error': 'Cette demande n\'est plus en attente'},status=status.HTTP_400_BAD_REQUEST)
+        
+        friendship.status = 'accepted'
+        friendship.save()
+
+        serializer = FriendshipSerializer(friendship)
+        return Response(serializer.data, status=status.HTTP_200_OK)
