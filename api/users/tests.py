03bd1eab2375e7458885fc21ddafcdc2e62dfecb
Acancel <cancel.adrien@gmail.com>
2026-06-11 18:14:09 +0200

    TEST(api/users/tests.py) : add more users model's unit tests

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,5 +1,7 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from django.db import IntegrityError 
 
 User = get_user_model()
 
@@ -30,3 +32,26 @@ def test_user_status(basic_user):
 @pytest.mark.django_db
 def test_user_str(basic_user):
 	assert str(basic_user) == "test@example.com"
+
+@pytest.mark.django_db
+def test_user_email_uniqueness(basic_user):
+	with pytest.raises(IntegrityError):
+		User.objects.create_user(
+			email="test@example.com",
+			username="anotheruser",
+			password="Anotherpassword123"
+		)
+
+@pytest.mark.django_db
+def test_user_mail_authentication(basic_user):
+	user = authenticate(
+		email="test@example.com",
+		password="Testpassword123"
+	)
+	assert user is not None
+
+@pytest.mark.django_db
+def test_user_password_stored_as_hash(basic_user):
+	assert basic_user.password != "Testpassword123"
+	assert basic_user.check_password("Testpassword123") == True
+
